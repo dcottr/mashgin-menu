f3094f30eaac9a3a009cb437998be319d2633569
David Cottrell <davidrcottrell@gmail.com>
2025-02-09 10:04:18 -0800

    Make quantity picker handle blank state better & have its own +/-

--- src/app/_components/quantityPicker.tsx
+++ src/app/_components/quantityPicker.tsx
@@ -1,19 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CloseIcon from "@mui/icons-material/Close";
 import styles from "~/app/index.module.css";
+import { set } from "zod";
 
 export default function QuantityPicker(props: {
   quantity: number;
   setQuantity: (value: number) => void;
 }) {
-  // Use isEditing to not switch the input field mid-typing it
+  // Use isEditing & local quantity state to not switch the input field mid-typing
   const [isEditing, setIsEditing] = useState(false);
+  const [quantity, setQuantity] = useState<number | undefined>(props.quantity);
+
+  useEffect(() => {
+    if (isEditing && quantity === undefined) {
+      // The user cleared the input field, don't update the parent state yet
+      return;
+    }
+    if (quantity !== props.quantity) {
+      props.setQuantity(quantity ?? 0);
+    }
+  }, [isEditing, quantity, props.quantity]);
 
   return props.quantity == 0 && !isEditing ? (
     <button
       style={{ height: "2.5rem", minWidth: "7.5rem" }}
       className={styles.styledButton}
-      onClick={() => props.setQuantity(1)}
+      onClick={() => setQuantity(1)}
     >
       + Add to Cart
     </button>
@@ -27,19 +39,69 @@ export default function QuantityPicker(props: {
         gap: "0.5rem",
       }}
     >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          alignItems: "center",
+          gap: "1px",
+        }}
+      >
+        <button
+          className={styles.styledButton}
+          style={{
+            width: "1rem",
+            height: "1rem",
+            fontSize: "0.8rem",
+            border: "0px",
+            borderRadius: "0",
+            padding: "0 0",
+          }}
+          onClick={() => {
+            setIsEditing(false); // Even if you're focused, trigger the changes when clicking this
+            setQuantity((quantity ?? 0) + 1);
+          }}
+        >
+          +
+        </button>
+        <button
+          className={styles.styledButton}
+          style={{
+            width: "1rem",
+            height: "1rem",
+            fontSize: "0.8rem",
+            border: "0px",
+            borderRadius: "0",
+            padding: "0 0",
+          }}
+          onClick={() => {
+            setIsEditing(false); // Even if you're focused, trigger the changes when clicking this
+            setQuantity((quantity ?? 1) - 1);
+          }}
+        >
+          -
+        </button>
+      </div>
       <input
-        type="number"
         className={styles.quantityPicker}
         name="amount"
-        min="0"
-        value={props.quantity}
-        onChange={(e) => props.setQuantity(+e.target.value)}
+        value={quantity ?? ""}
+        onChange={(e) => {
+          // Only allow numbers this way instead of input type="number" so we can handle empty strings
+          if (!/^\d*$/.test(e.target.value)) {
+            return;
+          }
+          setQuantity(
+            e.target.value === "" ? undefined : Number(e.target.value)
+          );
+        }}
         onFocus={() => setIsEditing(true)}
         onBlur={() => setIsEditing(false)}
       />
       <button
         className={styles.borderlessButton}
-        onClick={(e) => props.setQuantity(0)}
+        onClick={(e) => setQuantity(0)}
       >
         <CloseIcon style={{ color: "red" }} />
       </button>
